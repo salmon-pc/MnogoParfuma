356d59301f85207dd2d9b74ff2b33f033d7b81a2
hipahopa <aleks92-92@yandex.ru>
2014-10-16 11:50:54 +0600

    Полностью главная. Косяк с блюром

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 $(function () {
+    /*inits*/
+    initProductSliders();
+
     $(document).on("click", ".main_slider_block .prev", function () {
         var button = $(this);
         var visible_obj_img = $(".main_slider_block .slides_wrapper .slide:visible");
@@ -41,5 +44,67 @@ $(function () {
         new_visible_obj_content.fadeIn(500);
         return false;
     });
+
+    $(document).on('click',".item_slider_block .prev",function(){
+        var wrapper = $(this).parent(".products_slider");
+        var in_wrapper = wrapper.find(".in_wrapper");
+        var logo_wrap_inner_count = in_wrapper.find(".slide").length;
+        var logo_visible_elements = 4;
+        if(in_wrapper.css('margin-left')!=='0px')
+        {
+            var firsNotMuted = in_wrapper.find(".slide:not(.muted):eq(0)");
+            var lastNotMuted = in_wrapper.find(".slide:not(.muted):eq(3)");
+            firsNotMuted.prev(".slide").removeClass("muted");
+            lastNotMuted.addClass("muted");
+            in_wrapper.animate({
+                marginLeft:'+=255'
+            },100);
+        }
+        else{
+            in_wrapper.find(".slide").addClass('muted');
+            in_wrapper.find(".slide:gt("+(logo_wrap_inner_count - 5)+")").removeClass("muted");
+            in_wrapper.animate({
+                marginLeft:-(logo_wrap_inner_count-logo_visible_elements)*255
+            },100);
+        }
+        return false;
+    });
+    $(document).on('click',".item_slider_block .next",function(){
+        var wrapper = $(this).parent(".products_slider");
+        var in_wrapper = wrapper.find(".in_wrapper");
+        var logo_wrap_inner_count = in_wrapper.find(".slide").length;
+        var logo_visible_elements = 4;
+        if(in_wrapper.css('margin-left')!=="-"+(logo_wrap_inner_count-logo_visible_elements)*255+"px")
+        {
+            var firsNotMuted = in_wrapper.find(".slide:not(.muted):eq(0)");
+            var lastNotMuted = in_wrapper.find(".slide:not(.muted):eq(3)");
+            firsNotMuted.addClass("muted");
+            lastNotMuted.next(".slide").removeClass("muted");
+            in_wrapper.animate({
+                marginLeft:'-=255'
+            },100);
+        }
+        else{
+            in_wrapper.find(".slide").addClass('muted');
+            in_wrapper.find(".slide:lt(4)").removeClass("muted");
+            in_wrapper.animate({
+                marginLeft:0
+            },100);
+        }
+        return false;
+    });
+
+
+
+
 });
+
+function initProductSliders(){
+    $(".item_slider_block").each(function(){
+        var wrap = $(this);
+        var inwrap = wrap.find(".in_wrapper");
+        inwrap.width(inwrap.find(".slide").length * 255);
+        inwrap.find(".slide::gt(3)").addClass("muted");
+    });
+}
 
